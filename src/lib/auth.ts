742ab1e8d0f531defd234a43bbbf8bba0d92bd47
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2026-06-10 22:28:52 +0600

    fix: auth base url fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,10 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
 import * as schema from "./auth-schema";
 
+const authUrl = new URL(
+  process.env.BETTER_AUTH_URL || "http://localhost:3000/api/auth",
+);
+
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
@@ -15,4 +19,5 @@ export const auth = betterAuth({
         process.env.GOOGLE_CLIENT_SECRET || "placeholder_client_secret",
     },
   },
+  trustedOrigins: [authUrl.origin, `${authUrl.protocol}//www.${authUrl.host}`],
 });
